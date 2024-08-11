d79ccaa589bbcd85166bbd092d1577239c56dd61
Parth Dave <ilaxo.prth.dv@gmail.com>
2024-08-11 21:50:38 +0530

    New Project

---
+++ LittlestLemon/LittlestLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittlestLemonApp.swift
+//  LittlestLemon
+//
+//  Created by ilaxoM1 on 11/08/24.
+//
+
+import SwiftUI
+
+@main
+struct LittlestLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
